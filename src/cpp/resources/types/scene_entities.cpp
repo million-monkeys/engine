27da504dad1be3cfbf8860e8464e8232f06ac63f
Dan Kersten <danielytics@users.noreply.github.com>
2022-05-12 13:48:39 +0100

    Add ability to set entity groups to scene loader

--- src/cpp/resources/types/scene_entities.cpp
+++ src/cpp/resources/types/scene_entities.cpp
@@ -10,6 +10,27 @@ resources::types::SceneEntities::SceneEntities(core::Engine* engine)
 {
 }
 
+void addGroups (entt::registry& registry, const TomlValue& component, entt::entity entity)
+{
+    if (component.contains("groups")) {
+        auto& groups = component.at("groups");
+        if (groups.is_array()) {
+            for (auto& group_name : groups.as_array()) {
+                if (group_name.is_string()) {
+                    auto& storage = registry.storage<core::EntityGroup>(entt::hashed_string::value(group_name.as_string().str.c_str()));
+                    storage.emplace(entity);
+                } else {
+                    spdlog::warn("[scenes] Group name must be string");
+                }
+            }
+        } else {
+            spdlog::warn("[scenes] Group component's \"groups\" field must be an array ofgroup names");
+        }
+    } else {
+        spdlog::warn("[scenes] Group component must contain \"groups\" field");
+    }
+}
+
 bool resources::types::SceneEntities::load (million::resources::Handle handle, const std::string& filename)
 {
     EASY_FUNCTION(profiler::colors::Teal300);
@@ -28,7 +49,10 @@ bool resources::types::SceneEntities::load (million::resources::Handle handle, c
                     auto entity_id = prototype_registry.create();
                     prototype_registry.emplace<core::EntityPrototypeID>(entity_id, entt::hashed_string::value(name.c_str()));
                     for (const auto& [name_str, component]  : entity.as_table()) {
-                        if (name_str != "_name_") {
+                        if (name_str == "group") {
+                            // Add entity to groups
+                            addGroups(prototype_registry, component, entity_id);
+                        } else if (name_str != "_name_") {
                             SPDLOG_TRACE("[scenes] Adding component to prototype entity {}: {}", name, name_str);
                             toml::value value = component;
                             m_engine->loadComponent(prototype_registry, entt::hashed_string{name_str.c_str()}, entity_id, reinterpret_cast<const void*>(&value));
@@ -48,8 +72,14 @@ bool resources::types::SceneEntities::load (million::resources::Handle handle, c
                 SPDLOG_TRACE("[scenes] Creating new entity: {}", entt::to_integral(entity_id));
                 for (const auto& [name_str, component]  : entity.as_table()) {
                     SPDLOG_TRACE("[scenes] Adding component to entity {}: {}", entt::to_integral(entity_id), name_str);
-                    toml::value value = component;
-                    m_engine->loadComponent(scene_registry, entt::hashed_string{name_str.c_str()}, entity_id, reinterpret_cast<const void*>(&value));
+                    if (name_str == "group") {
+                        // Add entity to groups
+                        addGroups(scene_registry, component, entity_id);
+                    } else {
+                        toml::value value = component;
+                        m_engine->loadComponent(scene_registry, entt::hashed_string{name_str.c_str()}, entity_id, reinterpret_cast<const void*>(&value));
+                    }
+                    
                 }
             }
         }
